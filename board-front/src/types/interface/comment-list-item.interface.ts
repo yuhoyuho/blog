d6f3bdcefd8b1b0847096a4251cdb6f106576b9a
yuhoyuho <leeyuho123@naver.com>
2025-02-26 17:33:31 +0900

    인터페이스 리스트 추가 작성

---
+++ board-front/src/types/interface/comment-list-item.interface.ts
@@ -0,0 +1,6 @@
+export default interface CommentListItem {
+    nickname : String;
+    profileImage : String | null;
+    writeDatetime : String;
+    content : String;
+}
---
+++ board-front/src/types/interface/favorite-list-item.interface.ts
@@ -0,0 +1,5 @@
+export default interface FavoriteListItem {
+    email : String;
+    nickname : String;
+    profileImage : String | null;
+}
