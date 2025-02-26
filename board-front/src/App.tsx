e674d1dd80bd13db7ae3fd645870af791a0705da
yuhoyuho <leeyuho123@naver.com>
2025-02-26 23:38:06 +0900

    상위 3개 게시물 아이템

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import BoardItem from 'components/BoardItem';
-import { latestBoardListMock } from 'mocks';
+import { latestBoardListMock, top3BoardListMock } from 'mocks';
+import Top3Item from 'components/Top3Item';
 
 function App() {
   return (
     <>
-      {latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem}/>)}
+      <div style={{ display: 'flex', justifyContent: 'center', gap: '24px'}}>
+        {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
+      </div>
     </>
   );
 }
 
 export default App;
+
+
+// {latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem}/>)}
