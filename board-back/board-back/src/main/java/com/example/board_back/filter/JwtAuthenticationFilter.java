b2c252415457972f6db7f27851957e8f0bae5a7e
yuhoyuho <leeyuho123@naver.com>
2025-02-26 14:56:02 +0900

    back, 필터 추가

--- board-back/board-back/src/main/java/com/example/board_back/filter/JwtAuthenticationFilter.java
+++ board-back/board-back/src/main/java/com/example/board_back/filter/JwtAuthenticationFilter.java
@@ -6,6 +6,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -20,6 +27,36 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = parseBearerToken(request);
+
+        try {
+            if(token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            String email = jwtProvider.validate(token);
+
+            if(email == null) {
+                filterChain.doFilter(request, response);
+            }
+
+            // 인증 요청에 대한 세부정보 설정 (웹 인증 세부정보 소스)
+            AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, null, AuthorityUtils.NO_AUTHORITIES);
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            // Context에 등록을 위해 Context 생성
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(authenticationToken);
+
+            // 외부에서 사용하기 위해 holder에 context를 담아줌
+            SecurityContextHolder.setContext(securityContext);
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        // 다음 필터로 전달
+        filterChain.doFilter(request, response);
 
     }
 
