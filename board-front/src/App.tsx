c1cfed7892d7788100ae8110bff13c3a796f8a57
yuhoyuho <leeyuho123@naver.com>
2025-02-27 17:20:33 +0900

    댓글 리스트, 좋아요 리스트 추가

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import BoardItem from 'components/BoardItem';
-import { latestBoardListMock, top3BoardListMock } from 'mocks';
+import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
 import Top3Item from 'components/Top3Item';
+import CommentItem from 'components/CommentItem';
+import FavoriteItem from 'components/FavoriteItem';
 
 function App() {
   return (
     <>
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '24px'}}>
-        {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
-      </div>
+    <div style={{display: 'flex', columnGap: '30px', rowGap: '20px'}}>
+      {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem} />)}
+    </div>
     </>
   );
 }
@@ -18,3 +20,6 @@ export default App;
 
 
 // {latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem}/>)}
+{/* <div style={{ display: 'flex', justifyContent: 'center', gap: '24px'}}>
+        {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
+      </div> */}
