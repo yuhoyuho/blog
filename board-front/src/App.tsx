97ca21cb095de40d2bd78040660bfc3ff781a2ba
yuhoyuho <leeyuho123@naver.com>
2025-02-27 17:56:19 +0900

    로그인 inputbox 작업

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -5,13 +5,12 @@ import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMo
 import Top3Item from 'components/Top3Item';
 import CommentItem from 'components/CommentItem';
 import FavoriteItem from 'components/FavoriteItem';
+import InputBox from 'components/InputBox';
 
 function App() {
   return (
     <>
-    <div style={{display: 'flex', columnGap: '30px', rowGap: '20px'}}>
-      {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem} />)}
-    </div>
+      <InputBox/>
     </>
   );
 }
@@ -23,3 +22,6 @@ export default App;
 {/* <div style={{ display: 'flex', justifyContent: 'center', gap: '24px'}}>
         {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
       </div> */}
+    //   <div style={{display: 'flex', columnGap: '30px', rowGap: '20px'}}>
+    //   {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem} />)}
+    // </div>
