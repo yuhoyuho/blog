3cc76c46e0000ad51604a16058c11ffc26c98ee3
yuhoyuho <leeyuho123@naver.com>
2025-03-03 17:59:24 +0900

    파일 업로드, 불러오기 API 구현

---
+++ board-back/board-back/src/main/java/com/example/board_back/service/FileService.java
@@ -0,0 +1,10 @@
+package com.example.board_back.service;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileService {
+
+    String upload(MultipartFile file);
+    Resource getImage(String fileName);
+}
---
+++ board-back/board-back/src/main/java/com/example/board_back/service/implement/FileServiceImplement.java
@@ -0,0 +1,58 @@
+package com.example.board_back.service.implement;
+
+import com.example.board_back.service.FileService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
+
+@Service
+public class FileServiceImplement implements FileService {
+
+    @Value("${file.path}")
+    private String filePath;
+
+    @Value("${file.url}")
+    private String fileUrl;
+
+    @Override
+    public String upload(MultipartFile file) {
+
+        if(file.isEmpty()) return null; // 빈 파일이면 null 반환
+
+        String originalFilename = file.getOriginalFilename();
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString();
+        String saveFileName = uuid + extension;
+        String savePath = filePath + saveFileName;
+
+        try {
+            file.transferTo(new File(savePath));
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        String url = fileUrl + saveFileName;
+        return url;
+    }
+
+    @Override
+    public Resource getImage(String fileName) {
+
+        Resource resource = null;
+
+        try {
+            resource = new UrlResource("file:" + filePath + fileName);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return resource;
+    }
+}
