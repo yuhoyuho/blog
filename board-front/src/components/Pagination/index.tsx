268be558cb5ee88697bc6904f375e46126365d0c
yuhoyuho <leeyuho123@naver.com>
2025-03-04 22:40:59 +0900

    게시물 상세보기 하단 스타일 작성

--- board-front/src/components/Pagination/index.tsx
+++ board-front/src/components/Pagination/index.tsx
@@ -6,6 +6,6 @@ export default function Pagination() {
 
   //      render : 페이지네이션 컴포넌트 렌더링       //
   return (
-    <div>pagination</div>
+    <div></div>
   )
 }
--- board-front/src/views/Board/Deatail/index.tsx
+++ board-front/src/views/Board/Deatail/index.tsx
@@ -110,7 +110,7 @@ export default function BoardDetail() {
             <div className='board-detail-bottom-comment-pagination-box'>
               <Pagination />
             </div>
-            <div className='board-detail-bottom-comment-input-container'>
+            <div className='board-detail-bottom-comment-input-box'>
               <div className='board-detail-bottom-comment-input-container'>
                 <textarea className='board-detail-bottom-comment-textarea' placeholder='댓글을 작성해주세요.' />
                 <div className='board-detail-bottom-comment-button-box'>
