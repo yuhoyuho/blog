75b3740297e36974939dea8219304ae0f11865e5
yuhoyuho <leeyuho123@naver.com>
2025-03-11 22:03:55 +0900

    페이징 처리 컴포넌트 적용

--- board-front/src/views/Board/Deatail/index.tsx
+++ board-front/src/views/Board/Deatail/index.tsx
@@ -18,6 +18,7 @@ import { DeleteBoardResponseDto, GetCommentListResponseDto, GetFavoriteListRespo
 import dayjs from 'dayjs';
 import { useCookies } from 'react-cookie';
 import { PostCommentRequestDto } from 'apis/request/board';
+import { usePagination } from 'hooks';
 
 //      component : 게시물 상세 화면 컴포넌트        //
 export default function BoardDetail() {
@@ -178,11 +179,20 @@ export default function BoardDetail() {
     //    state : 댓글 textarea 참조 상태   //
     const commentRef = useRef<HTMLTextAreaElement | null>(null);
 
+    //    state : 페이지네이션 관련 상태    //
+    const {
+      currentPage,
+      setCurrentPage,
+      currentSection,
+      setCurrentSection,
+      viewList,
+      viewPageList,
+      totalSection,
+      setTotalList
+    } = usePagination<CommentListItem>(3);
+
     //    state : 좋아요 리스트 상태    //
     const [favoriteList, setFavoriteList] = useState<FavoriteListItem[]>([]);
-
-    //    state : 댓글 리스트 상태    //
-    const [commentList, setCommentList] = useState<CommentListItem[]>([]);
 
     //    state : 좋아요 상태   //
     const [isFavorite, setFavorite] = useState<boolean>(false);
@@ -195,6 +205,9 @@ export default function BoardDetail() {
 
     //    state : 댓글 상태   //
     const [comment, setComment] = useState<string>('');
+
+    //    state : 전체 댓글 개수 상태   //
+    const [totalCommentCount, setTotalCommentCount] = useState<number>(0);
 
     //    function : getFavoriteListResponse 처리 함수    //
     const getFavoriteListResponse = (responseBody : GetFavoriteListResponseDto | ResponseDto | null) => {
@@ -226,7 +239,8 @@ export default function BoardDetail() {
       if(code !== 'SU') return;
 
       const {commentList} = responseBody as GetCommentListResponseDto;
-      setCommentList(commentList);
+      setTotalList(commentList);
+      setTotalCommentCount(commentList.length);
     }
 
     //    function : putFavoriteResponse 처리 함수    //
@@ -326,7 +340,7 @@ export default function BoardDetail() {
               <div className='icon-button'>
                 <div className='icon comment-icon'></div>
               </div>
-              <div className='board-detail-bottom-button-text'>{`댓글 ${commentList.length}`}</div>
+              <div className='board-detail-bottom-button-text'>{`댓글 ${totalCommentCount}`}</div>
               <div className='icon-button' onClick={onShowCommentClickHandler}>
                 {showComment ? 
                 <div className='icon up-light-icon'></div> : 
@@ -348,14 +362,21 @@ export default function BoardDetail() {
           {showComment &&
           <div className='board-detail-bottom-comment-box'>
             <div className='board-detail-bottom-comment-container'>
-              <div className='board-detail-bottom-comment-title'>{'댓글 '}<span className='emphasis'>{12}</span></div>
+              <div className='board-detail-bottom-comment-title'>{'댓글 '}<span className='emphasis'>{totalCommentCount}</span></div>
               <div className='board-detail-bottom-comment-list-container'>
-                {commentList.map(item => <CommentItem commentListItem={item}/>)}
+                {viewList.map(item => <CommentItem commentListItem={item}/>)}
               </div>
             </div>
             <div className='divider'></div>
             <div className='board-detail-bottom-comment-pagination-box'>
-              <Pagination />
+              <Pagination
+                currentPage={currentPage}
+                currentSection={currentSection}
+                setCurrentPage={setCurrentPage}
+                setCurrentSection={setCurrentSection}
+                viewPageList={viewPageList}
+                totalSection={totalSection}
+              />
             </div>
             {loginUser !== null &&
             <div className='board-detail-bottom-comment-input-box'>
